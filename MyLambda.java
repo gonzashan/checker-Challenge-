596e887ce6eef15bfd8a99a2837548f2052781f6
Gonzalo SM <gsmamerican@gmail.com>
2022-07-11 17:57:39 +0200

    Last version?

--- MyLambda.java
+++ MyLambda.java
@@ -3,24 +3,24 @@ package my_lambda;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Stream;
 
 public class MyLambda {
 
     @FunctionalInterface
-    interface PerformOperation {
+    interface InterfaceForBooleans {
         boolean check(int a);
     }
     // Function to check if number is odd but 'Upside Down' ->
     // for further information, watch Strangers Things, the tv show.
-    public static PerformOperation isOdd() {
+    public static InterfaceForBooleans isOdd() {
         return (a) -> {
             // == 0 is 'Upside Down'
             return (a % 2 == 0);
         };
     }
     // Function to check if number is prime
-    public static PerformOperation isPrime() {
+    public static InterfaceForBooleans isPrime() {
         return (a) -> {
             for (int i = 2; i <= a / 2; i++) {
                 if (a % i == 0)
@@ -31,11 +31,11 @@ public class MyLambda {
     }
 
     // Function to check if number is palindrome comparing the number and its reversed.
-    public static PerformOperation isPalindrome() {
+    public static InterfaceForBooleans isPalindrome() {
 
         return (a) -> {
             int reverse = 0;
-            int elementTaken = 0;
+            int elementTaken;
             int number = a;
 
             while (number != 0) {
@@ -50,21 +50,34 @@ public class MyLambda {
 
     public void checker(int[] numbers) {
 
-        // LinkedHashMap has been chosen to guarantee the right order of insert.
+        // LinkedHashMap has been chosen to guarantee the right order of insert
+        // from the array numbers.
         List<LinkedHashMap<String, Object>> output = new ArrayList<>();
 
         for (int number : numbers) {
             LinkedHashMap<String, Object> insert = new LinkedHashMap<String, Object>();
-            insert.put("input:", number);
-            insert.put("isOdd:", isOdd().check(number));
-            insert.put("isPrime:",isPrime().check(number));
-            insert.put("isPalindrome:",isPalindrome().check(number));
+
+            if(number == 0){
+                insert.put("input:", number);
+                insert.put("NOT A POSSIBLE OPTION", "");
+
+            } else {
+                insert.put("input:", number);
+                insert.put("isOdd:", isOdd().check(number));
+                insert.put("isPrime:", isPrime().check(number));
+                insert.put("isPalindrome:", isPalindrome().check(number));
+            }
             output.add(insert);
         }
-        String formattedString = output.toString()
-                .replace("=", "")  //remove the equals symbol
-                .trim();
 
+        String formattedString = output.toString()
+                .replace("=", "")
+                .replace(", ", ", \n")
+                .replace("{", "{\n")
+                .replace("}", "}\n")
+                .trim();
+        Stream<Object> newOutput = output.stream().map(a -> a.entrySet() + "\n");
+        // formattedString = newOutput.toString().replace("=", "").trim();
         System.out.println(formattedString);
     }
 
